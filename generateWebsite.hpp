963259bbca8b58325c8d19a58639a263bd49a137
Ketoisc <brooke.bui@student.adelaide.edu.au>
2025-01-25 12:59:09 +1030

    website text

---
+++ generateWebsite.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class generateWebsite {
+    public:
+        string firstName;
+        string lastName;
+        string title;
+
+        string yearPublished;
+
+        string URL;
+
+        string yearViewed;
+        string monthViewed;
+        string dayViewed;
+
+        string webName;
+        string publisher;
+
+        string reference;
+
+
+        string generate();
+
+
+};
+
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "generateWebsite.cpp"
+#include "generateWebsite.hpp"
+
+int main() {
+    generateWebsite site;
+    site.generate();
+    return 0;
+}
